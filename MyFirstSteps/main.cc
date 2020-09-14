259ed98e6b48a2269f83dc2879a4c360705edab8
inform <inform@Class-03.kl9.uspu.ru>
2020-09-14 16:00:45 +0500

    Теперь она ьабулирует синус

--- MyFirstSteps/main.cc
+++ MyFirstSteps/main.cc
@@ -1,7 +1,22 @@
 #include <iostream>//подключение заголовочных файлов
+#include <cmath>
 int main()
 {
-	std::cout << "Всем ПРИВЕТ!" << std::endl;
+	std::cout << "Будем табулировать синус" << std::endl;
+
+	std::cout << "Введите начало и конец промежутка";
+	double start, end;
+	std::cin >> start>> end;
+
+	std::cout << "Введите шаг";
+	double step;
+	std::cin >> step;;
+
+	for (double x = start; x <= end; x += step){
+		double  y = sin(x);
+		std::cout << x << '|' << y << std::endl;
+	}
+
 	return 0;
 }
 
