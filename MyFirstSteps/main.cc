1f697dfbd877fad21f48654a580a1b30a78a82c9
inform <inform@Class-03.kl9.uspu.ru>
2020-09-14 17:08:03 +0500

    Квадратное уравнение с комплексными корнями

--- MyFirstSteps/main.cc
+++ MyFirstSteps/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>//подключение заголовочных файлов
 #include <cmath>
 #include <iomanip>//компановка или верстка
+#include <complex>
 
 constexpr double EPS = 0.0001;//предельная ошибка
 
@@ -10,23 +11,20 @@ int main()
 
 	std::cout << "ax^2+bx+c=0" << std::endl;
 	std::cout << "Введите коэффициенты a, b, c";
-	double a, b, c;
-	std::cin << a << b << c;
+	std::complex <double> a, b, c;
+	std::cin >> a >> b >> c;
 
-	double d;
-	d = b*b - 4*a*c;
+	std::complex <double> d;
+	d = b*b - 4.*a*c;
 
-	if (d < -EPS){
-		std::cout << "Нет действительных корней" <<std::endl;
-	}
-	//else(d == 0){}// Ник4ошда так не делать!!! оно вещественное
-	else if (d < EPS){
-		std::cout << "x = " <<(-b/2/a) << std::endl;
+	//else(d == 0){}// Never do it!!! It is double
+	if (d.real() < EPS and d.real() > -EPS){//This is 0 part or segment!
+		std::cout << "x = " <<(-b/2. /a) << std::endl;
 	}
 	else{
 		std::cout <<
-				"x1 = " << ((-b-sqrt(d))/2 /a) << std::endl <<
-				"x2 = " << ((-b+sqrt(d))/2 /a) << std::endl;
+				"x1 = " << ((-b-sqrt(d))/2.  /a) << std::endl <<
+				"x2 = " << ((-b+sqrt(d))/2. /a) << std::endl;
 	}
 	return 0;
 }
