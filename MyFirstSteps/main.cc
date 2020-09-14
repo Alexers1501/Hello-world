28dd450654df1fa7898df862232d8dae18ce5f3e
inform <inform@Class-03.kl9.uspu.ru>
2020-09-14 16:44:43 +0500

    Решаем квадратное уравнение

--- MyFirstSteps/main.cc
+++ MyFirstSteps/main.cc
@@ -1,32 +1,33 @@
 #include <iostream>//подключение заголовочных файлов
 #include <cmath>
 #include <iomanip>//компановка или верстка
+
+constexpr double EPS = 0.0001;//предельная ошибка
+
 int main()
 {
-	std::cout << "Будем табулировать синус" << std::endl;
+	std::cout << "Будем решать квадратное уравнение" << std::endl;
 
-	std::cout << "Введите начало и конец промежутка";
-	double start, end;
-	std::cin >> start>> end;
+	std::cout << "ax^2+bx+c=0" << std::endl;
+	std::cout << "Введите коэффициенты a, b, c";
+	double a, b, c;
+	std::cin << a << b << c;
 
-	std::cout << "Введите шаг";
-	double step;
-	std::cin >> step;;
+	double d;
+	d = b*b - 4*a*c;
 
-	std::cout <<
-			"	x	|	sin(x)\n"
-			"-----------------\n";
-
-	std::cout << std::fixed;
-
-	for (double x = start; x <= end; x += step){
-		double  y = sin(x);
-		std::cout <<
-				std::setw(14) << std::setprecision(3) << x <<//setpreс сколько знаков после точки
-				'|' <<
-				std::setw(14) << std::setprecision(3) << y << std::endl;
+	if (d < -EPS){
+		std::cout << "Нет действительных корней" <<std::endl;
 	}
-
+	//else(d == 0){}// Ник4ошда так не делать!!! оно вещественное
+	else if (d < EPS){
+		std::cout << "x = " <<(-b/2/a) << std::endl;
+	}
+	else{
+		std::cout <<
+				"x1 = " << ((-b-sqrt(d))/2 /a) << std::endl <<
+				"x2 = " << ((-b+sqrt(d))/2 /a) << std::endl;
+	}
 	return 0;
 }
 
